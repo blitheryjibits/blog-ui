afba0d95bf1f0c9a2b1edf1f1d4bb3932966d9d2
robert thornton <robert.thornton92@gmail.com>
2025-11-14 12:22:04 +0200

    Feature: added UI grid component for amin features blog posts to go beneath the Hero section of landing page

---
+++ src/components/layout/FeatureGrid.tsx
@@ -0,0 +1,49 @@
+// design a grid layout with a large card on left and two smaller stacked cards on the right
+import FeatureBlogCard from "../ui/featureBlogCard";
+import SmallBlogCard from "../ui/smallBlogCard";
+import type { BlogCardProps } from "../ui/smallBlogCard"; 
+
+interface FeatureGridProps {
+    blog1: BlogCardProps
+    blog2: BlogCardProps
+    blog3: BlogCardProps
+    featureBlog: 1 | 2 | 3   // flag to indicate which blog is the main feature
+}
+
+export default function FeatureGrid({ blog1, blog2, blog3, featureBlog }: FeatureGridProps) {
+  // Put blogs into an array for easier manipulation
+  const blogs = [blog1, blog2, blog3]
+
+  // Pick the feature blog based on the flag
+  const feature = blogs[featureBlog - 1] || blogs[0]
+
+  // The rest are small blogs
+  const smallBlogs = blogs.filter((_, idx) => idx !== featureBlog - 1)
+
+  return (
+    <div className="grid grid-cols-1 gap-y-4 md:grid-cols-2 md:gap-y-0 md:gap-x-4 min-w-[200px]">
+      {/* Left: Feature blog card */}
+      <div className="col-span-1 row-span-2">
+        <FeatureBlogCard
+          title={feature.title}
+          excerpt={feature.excerpt}
+          imageUrl={feature.imageUrl}
+          imageAlt={feature.imageAlt || "Feature blog image"}
+        />
+      </div>
+
+      {/* Right: Two stacked small blog cards */}
+      <div className="grid grid-rows-2 col-span-1 gap-4">
+        {smallBlogs.map((blog, idx) => (
+          <SmallBlogCard
+            key={idx}
+            title={blog.title}
+            excerpt={blog.excerpt}
+            imageUrl={blog.imageUrl}
+            imageAlt={blog.imageAlt || "Small blog image"}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
