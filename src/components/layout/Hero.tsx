298869763f260aac29d203183176d4c2394c617d
robert thornton <robert.thornton92@gmail.com>
2025-11-16 06:27:13 +0200

    UI: changed font color from black or white to accent and secondary-bg variables from the index.css file

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -1,24 +1,23 @@
 import { Button } from "@/components/ui/button";
 export default function Hero() {
   return (
-    // bg-gradient-to-b from-muted/50 to-background
-        <section className="py-20 px-6 text-center bg-[url(/images/blog-hero-image.jpg)] bg-cover bg-center bg-fixed">
-            <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
+        <section className="py-20 px-6 text-center bg-[url(/images/blog-hero-image.jpg)] bg-cover bg-center bg-fixed bg-black/50 bg-blend-multiply">
+            <h1 className="text-4xl font-bold tracking-tight sm:text-5xl text-(--color-accent)">
             Code Curiosity and Coffee
             <br />
             My Developer Diary
             </h1>
-            <p className="mt-4 text-lg text-muted-foreground max-w-xl mx-auto">
+            <p className="mt-4 text-lg text-(--color-secondary-bg) max-w-xl mx-auto">
                 This blog is my code sandbox
             </p>
             {/* raw insights,  */}
-            <p>
+            <p className="text-(--color-accent)">
                 expect
             </p>
-            <div className="mt-6 flex justify-center gap-4">
-            <Button>Get Started</Button>
+            {/* <div className="mt-6 flex justify-center gap-4">
+            <Button>Sign up</Button>
             <Button variant="outline">View Posts</Button>
-            </div>
+            </div> */}
         </section>
     );
 }   
